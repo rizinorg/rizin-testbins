99d55449c88c731604ee98b0a936c823f32a5d4a
wargio <wargio@libero.it>
2021-11-18 13:07:53 +0100

    Add flirt elf x86 example sig & pac

---
+++ flirt/elf-x86/curl-example.c
@@ -0,0 +1,30 @@
+/*
+gcc -Os -o curl-example curl-example.c -Wl,-Bstatic -Lcurl/lib/ -lcurl -Wl,-Bdynamic -gz -lssl -lcrypto -ldl -lm -lz -lpthread -lcares -lidn2 -lnghttp2 -lzstd -lpsl -lbrotlidec 
+*/
+
+#include <stdio.h>
+#include <curl/curl.h>
+ 
+int main(void)
+{
+  CURL *curl;
+  CURLcode res;
+ 
+  curl = curl_easy_init();
+  if(curl) {
+    curl_easy_setopt(curl, CURLOPT_URL, "https://example.com");
+    /* example.com is redirected, so we tell libcurl to follow redirection */
+    curl_easy_setopt(curl, CURLOPT_FOLLOWLOCATION, 1L);
+ 
+    /* Perform the request, res will get the return code */
+    res = curl_easy_perform(curl);
+    /* Check for errors */
+    if(res != CURLE_OK)
+      fprintf(stderr, "curl_easy_perform() failed: %s\n",
+              curl_easy_strerror(res));
+ 
+    /* always cleanup */
+    curl_easy_cleanup(curl);
+  }
+  return 0;
+}
