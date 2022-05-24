91cf0ab71c2c980f85c30397d2c2712d02fca92c
Rot127 <unisono@quyllur.org>
2022-05-24 12:54:23 -0400

    Add asm test binary.

--- elf/ppc/build_tests.sh
+++ elf/ppc/build_tests.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
-printf "Build 64bit test binary\n\n"
+printf "Build 64bit rzil test binary\n\n"
 $(powerpc64-linux-gnu-as -a64 -mregnames ppc64_fp.S -o ppc64_fp)
+
+printf "Build 64bit asm test binary\n\n"
+$(powerpc64-linux-gnu-as -a64 -mregnames asm_tests.S -o asm_tests)
 
 #printf "Build 32bit test binary\n"
 #$(powerpc64-linux-gnu-as -a32 -mregnames *_32.s -o ppc_insn_tests_32)
