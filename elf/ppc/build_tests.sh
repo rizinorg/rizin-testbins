ebe6bc55595ed5d336c7fe367662d87ff47ed023
Rot127 <unisono@quyllur.org>
2022-06-20 07:53:11 -0400

    Add return statements to insn tests. Add clear gpr fcn.

--- elf/ppc/build_tests.sh
+++ elf/ppc/build_tests.sh
@@ -6,6 +6,9 @@ $(powerpc64-linux-gnu-as -a64 -mregnames ppc64_fp.S -o ppc64_fp)
 printf "Build 64bit asm test binary\n\n"
 $(powerpc64-linux-gnu-as -a64 -mregnames asm_tests.S -o asm_tests)
 
+printf "Build 64bit qemu test binary\n\n"
+$(powerpc64-linux-musl-gcc -o qemu_ppc64_fp qemu_ppc64_fp_main.c ppc64_fp)
+
 #printf "Build 32bit test binary\n"
 #$(powerpc64-linux-gnu-as -a32 -mregnames *_32.s -o ppc_insn_tests_32)
 
