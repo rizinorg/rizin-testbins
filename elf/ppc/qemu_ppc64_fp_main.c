9123eb412c1ea0f9d1f748e3323641d970f56a4d
Rot127 <unisono@quyllur.org>
2022-06-28 10:23:43 -0400

    Add qemu runable test bin

---
+++ elf/ppc/qemu_ppc64_fp_main.c
@@ -0,0 +1,29 @@
+
+extern void add_insns();
+extern void sub_insns();
+extern void store_insns();
+extern void load_insns();
+extern void compare_insns();
+extern void branch_insns();
+extern void rotate_insns();
+extern void logical_insns();
+extern void set_gpr_to_0();
+
+int main() {
+    add_insns();
+    set_gpr_to_0();
+    sub_insns();
+    set_gpr_to_0();
+    store_insns();
+    set_gpr_to_0();
+    load_insns();
+    set_gpr_to_0();
+    compare_insns();
+    set_gpr_to_0();
+    rotate_insns();
+    set_gpr_to_0();
+    logical_insns();
+    branch_insns();
+    set_gpr_to_0();
+    return 0;
+}
