86dcc00797cdbf12a66918c78ad042a581b0a478
Rot127 <unisono@quyllur.org>
2022-07-19 11:31:27 -0400

    Add help to gen_stats

--- elf/ppc/statistics/gen_stats.sh
+++ elf/ppc/statistics/gen_stats.sh
@@ -1,7 +1,14 @@
-#!/bin/sh
+#!/bin/bash
+if [[ "$1" == "-h" || "$2" == "--help" ]]; then
+    echo "$0 <bin> <log-file>"
+    exit
+fi
+
+if [[ $# -ne 2 ]]; then
+    echo "$0 <bin> <log-file>"
+    exit
+fi
 
 echo "rizin -qq -A $1 2> $1.err | sort -g | uniq -c | sort -g -r > $2"
-
 rizin -qq -A "$1" 2> "$1.err" | sort -g | uniq -c | sort -g -r > "$2"
-
 ./print_stats.py "$2"
