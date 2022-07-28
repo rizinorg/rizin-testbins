5fc20a2b6321fbd356d922ba81600e6146c8004f
Rot127 <unisono@quyllur.org>
2022-07-28 13:56:49 -0400

    Restructure test binaries to prevent segfaults.

--- elf/ppc/build_tests.sh
+++ elf/ppc/build_tests.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 printf "Build 64bit rzil test binary\n\n"
-$(powerpc64-linux-gnu-as -a64 -mregnames ppc64_fp.S -o ppc64_fp)
+$(powerpc64-linux-musl-as -a64 -mregnames ppc64_fp.S -o ppc64_fp)
 
 printf "Build 64bit asm test binary\n\n"
-$(powerpc64-linux-gnu-as -a64 -mregnames asm_tests.S -o asm_tests)
+$(powerpc64-linux-musl-as -a64 -mregnames asm_tests.S -o asm_tests)
 
 printf "Build 64bit le qemu test binary\n\n"
-$(powerpc64le-linux-gnu-gcc -Wa,-mregnames qemu_ppc64_fp_main.c ppc64_fp.S -o qemu_ppc64le_fp)
+$(powerpc64le-linux-gnu-gcc -static -Wa,-mregnames qemu_ppc64_fp_main.c ppc64_fp.S -o qemu_ppc64le_fp)
 
 #printf "Build 32bit test binary\n"
 #$(powerpc64-linux-gnu-as -a32 -mregnames *_32.s -o ppc_insn_tests_32)
--- elf/ppc/qemu_ppc64_fp_main.c
+++ elf/ppc/qemu_ppc64_fp_main.c
@@ -1,44 +1,12 @@
+#include <stdio.h>
 
-extern void add_insns();
-extern void branch_insns();
-extern void cr_logical_insns();
-extern void compare_insns();
-extern void div_mul_insns();
-extern void logical_insns();
-extern void read_set_spr();
-extern void rotate_insns();
-extern void set_gpr_to_0();
-extern void shift_insns();
-extern void special_insns();
-extern void store_insns();
-extern void sub_insns();
-extern void test_loads();
+extern void run_all_tests();
 
 int main() {
-    add_insns();
-    set_gpr_to_0();
-    sub_insns();
-    set_gpr_to_0();
-    store_insns();
-    set_gpr_to_0();
-    test_loads();
-    set_gpr_to_0();
-    compare_insns();
-    set_gpr_to_0();
-    rotate_insns();
-    set_gpr_to_0();
-    logical_insns();
-    set_gpr_to_0();
-    special_insns();
-    set_gpr_to_0();
-    shift_insns();
-    set_gpr_to_0();
-    read_set_spr();
-    set_gpr_to_0();
-    cr_logical_insns();
-    set_gpr_to_0();
-    div_mul_insns();
-    // set_gpr_to_0();
-    // branch_insns();
+    printf("run all\n");
+    asm (
+        "bl run_all_tests"
+    );
+    
     return 0;
 }
