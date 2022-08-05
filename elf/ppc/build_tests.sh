7ab59c2243d5033808668ebd4ad52f843a46745c
Rot127 <unisono@quyllur.org>
2022-08-05 13:14:25 -0400

    Move src files into repos `src` dir.

--- elf/ppc/build_tests.sh
+++ elf/ppc/build_tests.sh
@@ -2,14 +2,17 @@
 # SPDX-FileCopyrightText: 2022 Rot127 <unisono@quyllur.org>
 # SPDX-License-Identifier: LGPL-3.0-only
 
+CWD=$(pwd)
+cd ../../src/ppc-rzil
+
 printf "Build 64bit le asm test binary\n\n"
-$(powerpc64le-linux-gnu-as -a64 -mregnames asm_tests.S -o asm_tests)
+$(powerpc64le-linux-gnu-as -a64 -mregnames asm_tests.S -o "$CWD"/asm_tests)
 
 printf "Build 64bit le asm  pseudo fuzz test binary\n\n"
-$(powerpc64le-linux-gnu-gcc -static -Wa,-mregnames pseudo_fuzz_main.c pseudo_fuzz_tests.S -o pseudo_fuzz_tests)
+$(powerpc64le-linux-gnu-gcc -static -Wa,-mregnames pseudo_fuzz_main.c pseudo_fuzz_tests.S -o "$CWD"/pseudo_fuzz_tests)
 
 printf "Build 64bit le test binary\n\n"
-$(powerpc64le-linux-gnu-gcc -Ttext 0x100000 -static -Wa,-mregnames ppc_main.c ppc64.S -o ppc64le_uplifted)
+$(powerpc64le-linux-gnu-gcc -Ttext 0x100000 -static -Wa,-mregnames ppc_main.c ppc64.S -o "$CWD"/ppc64le_uplifted)
 
 printf "Build 32bit be test binary\n\n"
-$(powerpc-linux-gnu-gcc -Ttext 0x100000 -static -m32 -Wa,-mregnames ppc_main.c ppc32.S -o ppc32be_uplifted)
+$(powerpc-linux-gnu-gcc -Ttext 0x100000 -static -m32 -Wa,-mregnames ppc_main.c ppc32.S -o "$CWD"/ppc32be_uplifted)
