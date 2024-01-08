336497c035af9ef486d802212fa5c7347d3fdb83
Dhruv Maroo <dhruvmaru007@gmail.com>
2024-01-08 14:01:54 +0530

    Update test build script for PPC RzIL tests

--- elf/ppc/build_tests.sh
+++ elf/ppc/build_tests.sh
@@ -3,7 +3,7 @@
 # SPDX-License-Identifier: LGPL-3.0-only
 
 CWD=$(pwd)
-cd ../../src/ppc-rzil
+cd ../../src/rzil/ppc
 
 echo "* asm_tests"
 powerpc64le-linux-gnu-as -a64 -mregnames asm_tests.S -o "$CWD"/asm_tests
@@ -21,13 +21,13 @@ echo "* ppc32be_uplifted"
 powerpc-linux-gnu-gcc -Ttext 0x100000 -static -m32 -Wa,-mregnames ppc_main.c ppc32.S -o "$CWD"/ppc32be_uplifted
 
 echo "* emulateme-ppc32le"
-powerpcle-linux-musl-gcc -Ttext 0x100000 -Wl,-no-pie -static ../../src/ppc-rzil/emulateme.c -o "$CWD"/emulateme-ppc32le
+powerpcle-linux-musl-gcc -Ttext 0x100000 -Wl,-no-pie -static ../../src/rzil/emulateme.c -o "$CWD"/emulateme-ppc32le
 
 echo "* emulateme-ppc32be"
-powerpc-linux-gnu-gcc -Ttext 0x100000 -Wl,-no-pie -static ../../src/ppc-rzil/emulateme.c -o "$CWD"/emulateme-ppc32be
+powerpc-linux-gnu-gcc -Ttext 0x100000 -Wl,-no-pie -static ../../src/rzil/emulateme.c -o "$CWD"/emulateme-ppc32be
 
 echo "* emulateme-ppc64le"
-powerpc64le-linux-gnu-gcc -Ttext 0x100000 -Wl,-no-pie -static ../../src/ppc-rzil/emulateme.c -o "$CWD"/emulateme-ppc64le
+powerpc64le-linux-gnu-gcc -Ttext 0x100000 -Wl,-no-pie -static ../../src/rzil/emulateme.c -o "$CWD"/emulateme-ppc64le
 
 echo "* emulateme-ppc64be"
-powerpc64-linux-musl-gcc -Ttext 0x100000 -Wl,-no-pie -static ../../src/ppc-rzil/emulateme.c -o "$CWD"/emulateme-ppc64be
+powerpc64-linux-musl-gcc -Ttext 0x100000 -Wl,-no-pie -static ../../src/rzil/emulateme.c -o "$CWD"/emulateme-ppc64be
