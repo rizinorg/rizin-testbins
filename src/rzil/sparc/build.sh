82f7a1dac277b929f9a10dc8748604842bc01cf9
Rot127 <unisono@quyllur.org>
2025-07-17 14:50:17 -0500

    Extend test binary with most of Capstone's MC instructions.

--- src/rzil/sparc/build.sh
+++ src/rzil/sparc/build.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
 sparc64-linux-gnu-as -64 -o sparc64.o sparc64.s
-sparc64-linux-gnu-gcc -nostdlib -g -O0 sparc64.o -o sparc64_insn_all.bin main_nostd.c
+sparc64-linux-gnu-gcc -nostdlib -g -O0 sparc64.o -o sparc64_insn_all.bin _start.c
 
-sparc64-linux-gnu-gcc -nostdlib -g -O0 -o sparc64_emulateme.bin ../emulateme.nostd_2.c
+sparc64-linux-gnu-gcc -nostdlib -g -O0 -I. -o sparc64_emulateme.bin ../emulateme.nostd_2.c
 
 # sparc32-linux-gnu-as -32 -o sparc32.o sparc32.s
-# sparc32-linux-gnu-gcc -nostdlib sparc32.o -o sparc32_insn_all.bin main_nostd.c
+# sparc32-linux-gnu-gcc -nostdlib sparc32.o -o sparc32_insn_all.bin _start.c
 
 mv *.bin ../../../elf/sparc/
+ls -lh ../../../elf/sparc/*.bin
