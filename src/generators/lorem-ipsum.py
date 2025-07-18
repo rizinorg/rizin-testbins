b20b83da023daf82cbfeb8a80c2a89f3c7265004
Rot127 <unisono@quyllur.org>
2025-07-18 13:51:56 -0500

    Ensure UTF-16/32 strings are properly aligned to code point width

--- src/generators/lorem-ipsum.py
+++ src/generators/lorem-ipsum.py
@@ -30,6 +30,17 @@ SUPPORTED_LOCALES = {
     "ko": "Korean",
 }
 
+def enc_padding(enc) -> int:
+    match enc:
+        case "utf-8":
+            return 1
+        case "utf-16-be" | "utf-16-le":
+            return 2
+        case "utf-32-be" | "utf-32-le":
+            return 4
+        case _:
+            raise ValueError(f"{enc} not handled.")
+
 
 def generate_dummy_file(file_path, encodings, target_size, locale="en", noise=False):
     """
@@ -39,20 +50,22 @@ def generate_dummy_file(file_path, encodings, target_size, locale="en", noise=Fa
     max_words_per_chunk = 500
     output_files = list()
     for enc in encodings:
-        output_files.append((enc, open(file_path + "." + enc, "wb")))
+        pad = enc_padding(enc)
+        output_files.append((enc, pad, open(file_path + "." + enc, "wb")))
 
     written_size = 0
     while written_size < target_size:
         chunk = " ".join(fake.words(nb=random.randint(0, max_words_per_chunk))) + "\n"
         max_chunk_size = 0
         noise_size = random.randint(0, max_words_per_chunk)
-        for encoding, f in output_files:
+        for encoding, pad, f in output_files:
             bchunk = chunk.encode(encoding)
             chunk_size = len(bchunk)
             f.write(bchunk)
             max_chunk_size = max(max_chunk_size, chunk_size)
 
             if noise:
+                noise_size += (noise_size % pad)
                 f.write(random.randbytes(noise_size))
         if noise:
             written_size += noise_size
@@ -60,7 +73,8 @@ def generate_dummy_file(file_path, encodings, target_size, locale="en", noise=Fa
 
         if written_size % (10 * 1024 * 1024) < max_chunk_size:
             print(f"Progress: {written_size / target_size * 100:.1f}%", end="\r")
-    for _, f in output_files:
+    print()
+    for _, _, f in output_files:
         f.close()
 
 
