ec1b7e31fde40a7a960e4d7b76b4b882e7d0d07c
Rot127 <unisono@quyllur.org>
2025-08-14 10:50:01 -0500

    Add OpenBSD source

---
+++ src/rzil/sparc/build_openbsd.sh
@@ -0,0 +1,6 @@
+#!/bin/ksh
+# SPDX-FileCopyrightText: 2025 Rot127 <unisono@quyllur.org>
+# SPDX-License-Identifier: LGPL-3.0-only
+
+as -xarch=v9m -64 -I. -o sparc64_openbsd.o sparc64_openbsd.s
+gcc -g -O0 sparc64_openbsd.o -o sparc64_insn_openbsd.bin main.c
