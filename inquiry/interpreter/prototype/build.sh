86b7f77fe9fdabb550c621957d9f6322c7a4352c
Rot127 <unisono@quyllur.org>
2026-02-14 15:04:10 +0100

    Add more architectures and binary tests.

--- inquiry/interpreter/prototype/build.sh
+++ inquiry/interpreter/prototype/build.sh
@@ -1,23 +1,29 @@
-# SPDX-FileCopyrightText: 2024 Rot127 <unisono@quyllur.org>
+# SPDX-FileCopyrightText: 2026 Rot127 <unisono@quyllur.org>
 # SPDX-License-Identifier: LGPL-3.0-only
 
-echo "Compile x86 binaries"
-clang --static -o x86_icall_malloc icall_malloc.c
-# clang -c -o x86_unmapped_fcn_in_loop.o unmapped_fcn_in_loop.c
-# clang -c -o x86_discover_recurse.o discover_recurse.c
-# clang -c -o x86_paper_dep_example.o paper_dep_example.c
-# clang -c -o x86_post_simple_two_deps.o post_simple_two_deps.c
-# clang -c -O1 -o x86_post_loop_offsets.o post_loop_offsets.c
-# clang -c -o x86_post_pass_ref_across_proc.o post_pass_ref_across_proc.c
+echo "Compile x86 objects"
+clang --static -c -o x86_icall_malloc icall_malloc.c
+clang --static -c -o x86_unmapped_fcn_in_loop unmapped_fcn_in_loop.c
+clang --static -c -o x86_discover_recurse discover_recurse.c
+
+HEXAGON_SDK_TOOLS_BIN="/home/$USER/toolchains/Hexagon_SDK/6.4.0.2/tools/HEXAGON_Tools/19.0.04/Tools/bin/"
 
 echo "Compile Hexagon objects"
-/home/$USER/toolchains/Hexagon_SDK/6.4.0.2/tools/HEXAGON_Tools/19.0.04/Tools/bin/clang --static -c -O0 -o hexagon_icall_malloc icall_malloc.c
-# hexagon-unknown-linux-musl-clang -c -O0 -o hexagon_unmapped_fcn_in_loop.o unmapped_fcn_in_loop.c
-# hexagon-unknown-linux-musl-clang -c -O0 -o hexagon_discover_recurse.o discover_recurse.c
-# hexagon-unknown-linux-musl-clang -c -O0 -o hexagon_paper_dep_example.o paper_dep_example.c
-# hexagon-unknown-linux-musl-clang -c -O0 -o hexagon_post_simple_two_deps.o post_simple_two_deps.c
-# hexagon-unknown-linux-musl-clang -c -O0 -o hexagon_post_loop_offsets.o post_loop_offsets.c
-# hexagon-unknown-linux-musl-clang -c -O0 -o hexagon_post_pass_ref_across_proc.o post_pass_ref_across_proc.c
+$HEXAGON_SDK_TOOLS_BIN/clang --static -c -O0 -o hexagon_icall_malloc icall_malloc.c
+$HEXAGON_SDK_TOOLS_BIN/clang --static -c -O0 -o hexagon_unmapped_fcn_in_loop unmapped_fcn_in_loop.c
+$HEXAGON_SDK_TOOLS_BIN/clang --static -c -O0 -o hexagon_discover_recurse discover_recurse.c
 
 echo "Compile Sparc objects"
 sparc64-linux-gnu-gcc --static -c -O0 -o sparc_icall_malloc icall_malloc.c
+sparc64-linux-gnu-gcc --static -c -O0 -o sparc_unmapped_fcn_in_loop unmapped_fcn_in_loop.c
+sparc64-linux-gnu-gcc --static -c -O0 -o sparc_discover_recurse discover_recurse.c
+
+echo "Compile ARM objects"
+arm-none-eabi-gcc --static -c -O0 -o arm_icall_malloc icall_malloc.c
+arm-none-eabi-gcc --static -c -O0 -o arm_unmapped_fcn_in_loop unmapped_fcn_in_loop.c
+arm-none-eabi-gcc --static -c -O0 -o arm_discover_recurse discover_recurse.c
+
+echo "Compile Mips64 objects"
+mips64-linux-gnu-gcc --static -c -O0 -o mips64_icall_malloc icall_malloc.c
+mips64-linux-gnu-gcc --static -c -O0 -o mips64_unmapped_fcn_in_loop unmapped_fcn_in_loop.c
+mips64-linux-gnu-gcc --static -c -O0 -o mips64_discover_recurse discover_recurse.c
