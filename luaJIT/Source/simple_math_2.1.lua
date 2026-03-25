b504a97bc65c1a88d7ad88b072e6fa2be9936318
Arya H R <aryagowda177@gmail.com>
2026-03-25 09:10:28 +0000

    New luaJIT binaries: Test for integers and complex number

---
+++ luaJIT/Source/simple_math_2.1.lua
@@ -0,0 +1,11 @@
+local test_int = 1122334455667788LL
+
+local test_cmplx = 0 + 5.5i
+
+local data = {
+    int_val = test_int,
+    cplx_val = test_cmplx
+}
+
+print("Integer:", data.int_val)
+print("Complex:", data.cplx_val)
