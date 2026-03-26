ea72bec9fa59ca865c23d48eaff4e211148ac411
Divy Kumar Pandey <divykp24@iitk.ac.in>
2026-03-26 22:39:23 +0530

    elf: update test binaries to include IRELATIVE and GOT relocs

--- src/elf/tls_relocs_issue5259.c
+++ src/elf/tls_relocs_issue5259.c
@@ -1,9 +1,20 @@
 #include <stdio.h>
 
-// This forces TLS relocations (DTPMOD, DTPOFF, TPOFF)
+//TLS Relocations
 __thread int my_tls_var = 1337;
-__thread int my_tls_var2 = 42;
+extern __thread int ext_tls_var;
 
-int get_tls(){
-    return my_tls_var + my_tls_var2;
+//IRELATIVE (Indirect Functions)
+static int my_ifunc_impl() {return 42; }
+void* my_ifunc_resolver()  {return (void*)my_ifunc_impl; }
+int my_ifunc() __attribute__ (  (ifunc("my_ifunc_resolver"))  );
+
+//GOT and PLT 
+extern int external_var;
+extern void external_func();
+
+int do_everything() {
+    external_func(); // Triggers PLT
+    my_ifunc();      // This triggers IRELATIVE
+    return my_tls_var + ext_tls_var + external_var; // to trigger TLS and GOT
 }
